dd58913f8c0357c49b44ed64fb58424f8fb323c2
Omar <96790364+Omaralaac@users.noreply.github.com>
2024-04-25 21:33:59 +0200

    first commit

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ def region():
     json_data = request.json
     print(json_data)
 
-    # تحويل القيم إلى أرقام صحيحة
-    for key in json_data:
-        json_data[key] = int(json_data[key][0])
-
     pred = model.predict(np.array([list(json_data.values())]))
+
+    # تنسيق الاستجابة
     output = {}
     if int(pred) == 0:
         pred_pat = 'No Heart Disease'
     else:
         pred_pat = 'Has Heart Disease'
     output['prediction'] = pred_pat
+
+    # إرسال الاستجابة كتنسيق JSON
     return jsonify(output)
 
 
