fb366c67e4c24dd2da3c8014cdf76a2787f2eb39
Omar <96790364+Omaralaac@users.noreply.github.com>
2024-04-26 19:04:02 +0300

    first commit

--- app.py
+++ app.py
@@ -8,26 +8,36 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 model = pickle.load(open("model1.pkl", "rb"))
+@app.route('/')
+def home():
+    return 'Lets make a prediction'
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
-def region():
+def predict():
     json_data = request.json
     print(json_data)
 
+    data = [json_data[key] for key in json_data]
+    data_array = np.array(data).reshape(1, -1)
     pred = model.predict(np.array([list(json_data.values())]))
+    
+    proba = model.predict_proba(data_array)
+    proba_of_disease = proba[0][1] 
+    
+    prop =  round(proba_of_disease * 100, 2)
+    #prop["Risk rate"] = str(prop["Risk rate"]) + "%" 
 
-    # تنسيق الاستجابة
+
     output = {}
     if int(pred) == 0:
         pred_pat = 'No Heart Disease'
+        
     else:
         pred_pat = 'Has Heart Disease'
-    output['prediction'] = pred_pat
+        
+    output = pred_pat , prop
 
-    # إرسال الاستجابة كتنسيق JSON
     return jsonify(output)
-
-
 
 app.run(host='0.0.0.0', port=8081)
